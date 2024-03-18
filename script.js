eb97817397050cfaad1e404ac3af90cf07a3cbd2
Tristan Vidal <trsgot@gmail.com>
2024-03-18 16:52:29 +0000

    Refactor HTML and CSS code

--- script.js
+++ script.js
@@ -5,23 +5,23 @@ let timeoutId;
 textarea1.value = textarea2.value;
 
 textarea2.addEventListener('keydown', (event) => {
-  
+
   if (event.key === 'Tab') {
     event.preventDefault();
 
-    if(textarea1.value === '') return
+    if (textarea1.value === '') return
 
     textarea2.value = textarea1.value;
     return;
   }
-  textarea1.value=""
+  textarea1.value = ""
 });
 
 
 textarea2.addEventListener('input', function () {
   clearTimeout(timeoutId);
   timeoutId = setTimeout(async () => {
-    const url = 'https://api.typewise.ai/latest/completion/sentence_complete';
+    const url = 'https://completion.deno.dev/api/completion';
     const options = {
       method: 'POST',
       headers: {
@@ -38,15 +38,22 @@ textarea2.addEventListener('input', function () {
       const result = await response.json();
       if (result.predictions && result.predictions.length > 0) {
         const prediction = result.predictions[0].text;
-        const lastWordTextarea1 = textarea1.value.split(' ').pop(); // Obtener la última palabra de textarea1
-        const firstWordPrediction = prediction.split(' ')[0]; // Obtener la primera palabra de prediction
-        if (firstWordPrediction.includes(lastWordTextarea1)) {
-          const newText = textarea2.value.split(' ')
-          newText.pop()
-          textarea1.value = `${newText.join(' ')} ${prediction}`
+        // const lastWordTextarea1 = textarea1.value.split(' ').pop(); // Obtener la última palabra de textarea1
+        // const firstWordPrediction = prediction.split(' ')[0]; // Obtener la primera palabra de prediction
+        // if (firstWordPrediction.includes(lastWordTextarea1)) {
+        //   const newText = textarea2.value.split(' ')
+        //   newText.pop()
+        //   textarea1.value = `${newText.join(' ')} ${prediction}`
+        // } else {
+        //   textarea1.value = textarea2.value + prediction;
+        // }
+        //add space if there is no space at the end of textarea2
+        if (textarea2.value[textarea2.value.length - 1] !== ' ') {
+          textarea1.value = textarea2.value + ' ' + prediction;
         } else {
           textarea1.value = textarea2.value + prediction;
         }
+
       }
     } catch (error) {
       console.error(error);
